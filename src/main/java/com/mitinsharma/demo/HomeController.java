2bb32db423ac33294f7c5ce6352f31dba687b09f
Mitin Sharma <mitinsharma@Mitins-MBP.fios-router.home>
2022-01-17 18:46:40 -0500

    Setup Webapp

---
+++ src/main/java/com/mitinsharma/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.mitinsharma.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+ 
+	@RequestMapping("home")
+	//@ResponseBody
+	public String home() {
+		System.out.println("Home Controller");
+		return "home";
+	}
+}
