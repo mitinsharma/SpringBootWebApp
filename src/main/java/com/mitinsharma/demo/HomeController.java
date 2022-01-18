4b711fdb4aae4ec80868b9921fe6e93892b08083
Mitin Sharma <mitinsharma@Mitins-MBP.fios-router.home>
2022-01-17 19:08:59 -0500

    using SpringMVC to get Client Data

--- src/main/java/com/mitinsharma/demo/HomeController.java
+++ src/main/java/com/mitinsharma/demo/HomeController.java
@@ -1,26 +1,19 @@
 package com.mitinsharma.demo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
  
 	@RequestMapping("home")
 	//@ResponseBody
-	public String home(HttpServletRequest req, HttpServletResponse res) {
-		//System.out.println("Home Controller");
-		HttpSession session = req.getSession();
-		String name = req.getParameter("name");
-		System.out.println("Name: " + name);
-		//using request
-		//req.setAttribute("name", name);
-		//using session
-		session.setAttribute("name", name);
+	public String home(@RequestParam("name") String myName, HttpSession session) {
+		System.out.println("Name: " + myName);
+		session.setAttribute("name", myName);
 		return "home";
 	}
 }
